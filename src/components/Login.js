2c81c662b2ed8d8bf6f4aebe7dc0ec9026b72963
KuriatoV <kuriato.valentyn@gmail.com>
2017-02-15 17:08:52 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,7 +18,7 @@ class Login extends Component {
     next.loginSuccess && (browserHistory.push('/events'));
 
   }
-  componentDidUnmount(){
+  componentWillUnmount(){
     this.props.actions.resetToInitialstate;
   }
 
